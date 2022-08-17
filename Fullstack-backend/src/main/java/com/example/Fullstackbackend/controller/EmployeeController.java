e5d437a579bf09ea553995b460513418c5a2c2fc
988451835v <Kadnishedha@gmail.com>
2022-08-17 12:03:17 +0530

    Completing axios

--- Fullstack-backend/src/main/java/com/example/Fullstackbackend/controller/EmployeeController.java
+++ Fullstack-backend/src/main/java/com/example/Fullstackbackend/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.Fullstackbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.Fullstackbackend.model.Employee;
 import com.example.Fullstackbackend.repository.EmployeeRepository;
 
-
+@CrossOrigin(origins= "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
