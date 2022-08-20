06170e0b08338cfdfdd090aa43ed7222e4382077
988451835v <Kadnishedha@gmail.com>
2022-08-20 07:59:21 +0530

    Coonecting axios to the add emplyee form

--- react-frontend/src/components/CreateEmployeeComponent.jsx
+++ react-frontend/src/components/CreateEmployeeComponent.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class CreateEmployeeComponent extends Component {
     constructor(props){
         super(props)
             this.state ={
-
+                firstName: '',
+                lastName:'',
+                emailId:''
             }
+
+            this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this)
+            this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+            this.saveEmplyee = this.saveEmplyee.bind(this);
     }
+
+    saveEmplyee = (e) =>{
+        e.preventDefault();
+        let employee = {firstName:this.state.firstName,lastName:this.state.lastName,emailId:this.state.emailId};
+        console.log('employee => ' + JSON.stringify(employee));
+
+        EmployeeService.createEmployee(employee).then(res =>{
+            this.props.history.push('/employees')
+        })
+    }
+
+    changeFirstNameHandler = (event) =>{
+        this.setState({firstName:event.target.value});
+    }
+
+    changeLastNameHandler = (event) =>{
+        this.setState({lastName:event.target.value});
+    }
+
+    changeEmailHandler = (event) =>{
+        this.setState({emailId:event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('employees');
+    }
+
+    
     render() {
         return (
             <div>
@@ -31,6 +66,9 @@ class CreateEmployeeComponent extends Component {
                                             <input placeholder="Email Address" name="emailId" className="form-control" 
                                                 value={this.state.emailId} onChange={this.changeEmailHandler}/>
                                 </div>
+
+                                <button className="btn btn-success" onClick={this.saveOrUpdateEmployee}>Save</button>
+                                <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
                                
                                 </form>
                             </div>
